dbb345068cffc82a115cf1de59af165dcc12be8e
wilfredo <coclemmert@gmail.com>
2022-07-09 16:08:20 -0700

    feat:Railway

--- Utils/Utils.js
+++ Utils/Utils.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const instance = axios.create({
-    baseURL:"http://localhost:8888/.netlify/functions"
+    baseURL:"https://resplendent-choux-fc57b5.netlify.app"
 })
 
 
